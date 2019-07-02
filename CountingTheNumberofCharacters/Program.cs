8b0a448316f16ad19a60b86fe40c729c4ff6101f
A'Keem Drew <adrew0321@gmail.com>
2019-07-02 01:24:13 -0400

    Added new variables: userInput numberOfCharactersInUserInput | Added logic

--- CountingTheNumberofCharacters/Program.cs
+++ CountingTheNumberofCharacters/Program.cs
@@ -12,8 +12,21 @@ namespace CountingTheNumberofCharacters
         {
             // The code provided will create a program that prompts for an input string and displays output that shows the input string and the number of characters the string contains..
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+
+            Console.WriteLine("Counting The Number Of Characters \n");
+            Console.WriteLine("This program will take in the users input and count the number of characters in the users response \n");
+            Console.WriteLine("For Example: \n\n" +
+                "\t What is your input? Basketball \n" +
+                "\t Basketball has 10 characters. \n");
+
+            Console.WriteLine("---------------------------------------------------------------------- \n");
+
+            Console.Write("What is your input? ");
+            string userInput = Console.ReadLine().Trim();
+            int numberOfCharactersInUserInput = userInput.Length;
+
+            Console.WriteLine($"{userInput} has {numberOfCharactersInUserInput} characters \n");
+
 
         }
     }
