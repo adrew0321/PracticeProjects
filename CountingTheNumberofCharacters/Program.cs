e2882dbf2a5fad964294b13b768510e8bc657c03
A'Keem Drew <adrew0321@gmail.com>
2019-07-02 00:24:54 -0400

    quick edit

--- CountingTheNumberofCharacters/Program.cs
+++ CountingTheNumberofCharacters/Program.cs
@@ -10,7 +10,7 @@ namespace CountingTheNumberofCharacters
     {
         static void Main(string[] args)
         {
-            // The code provided will Create a program that prompts for an input string and displays output that shows the input string and the number of characters the string contains..
+            // The code provided will create a program that prompts for an input string and displays output that shows the input string and the number of characters the string contains..
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Console.WriteLine("Hello World!");
             Console.ReadKey();
