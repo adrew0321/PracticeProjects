be87456e5804679e15156c88085df43982bcc647
A'Keem Drew <adrew0321@gmail.com>
2019-07-02 16:58:25 -0400

    Added playAgain loop and exit program functionality

--- CountingTheNumberofCharacters/Program.cs
+++ CountingTheNumberofCharacters/Program.cs
@@ -15,7 +15,10 @@ namespace CountingTheNumberofCharacters
 
             string userInput;
             int numberOfCharactersInUserInput;
+            string playAgainResponse;
+            bool playAgain;
 
+            
             Console.WriteLine("Counting The Number Of Characters \n");
             Console.WriteLine("This program will take in the users input and count the number of characters in the users response \n");
             Console.WriteLine("For Example: \n\n" +
@@ -23,13 +26,35 @@ namespace CountingTheNumberofCharacters
                 "\t Basketball has 10 characters. \n");
             Console.WriteLine("---------------------------------------------------------------------- \n");
 
-            Console.Write("What is your input? ");
-            userInput = Console.ReadLine();
-            numberOfCharactersInUserInput = userInput.Length;
-            GetNumberOfCharacters( userInput,  numberOfCharactersInUserInput);
+            playAgain = true;
+            while (playAgain)
+            {
+                Console.Write("What is your input? ");
+                userInput = Console.ReadLine();
+                numberOfCharactersInUserInput = userInput.Length;
+                GetNumberOfCharacters(userInput, numberOfCharactersInUserInput);
 
-            Console.WriteLine($"\"{userInput}\" has {numberOfCharactersInUserInput} characters \n");
+                Console.WriteLine($"\"{userInput}\" has {numberOfCharactersInUserInput} characters \n");
 
+                Console.WriteLine("Would you like to play again? (Yes or No)");
+                playAgainResponse = Console.ReadLine().ToLower();
+
+                if (playAgainResponse == "yes" || playAgainResponse == "y")
+                {
+                    playAgain = true;
+                }
+                else if (playAgainResponse == "no" || playAgainResponse == "n" )
+                {
+                    playAgain = false;
+                    Console.WriteLine("Understood, thank you for playing!");
+                    System.Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("Input not accepted. Please try again...");
+                }
+
+            }
 
         }
 
