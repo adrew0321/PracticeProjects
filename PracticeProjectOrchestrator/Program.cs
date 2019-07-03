27da75858e07ebba6925227c88564f6ce4ebd750
A'Keem Drew <adrew0321@gmail.com>
2019-07-03 13:48:33 -0400

    Removed Python Project | Added PracticeProject Orchestrator

---
+++ PracticeProjectOrchestrator/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProjectOrchestrator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This page will host the routing for the projects. 
+            // From this project, you will be able to select from a list of projects
+            // Once the selection is made then you will be able to run that project
+
+        }
+    }
+}
