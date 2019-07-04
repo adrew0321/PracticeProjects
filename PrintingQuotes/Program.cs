c011b6d0451ea773756b1125c1b4f0c10ea4bbe2
A'Keem Drew <adrew0321@gmail.com>
2019-07-03 21:43:16 -0400

    Added functionality for program

--- PrintingQuotes/Program.cs
+++ PrintingQuotes/Program.cs
@@ -13,12 +13,24 @@ namespace PrintingQuotes
             // The code provided will prompts for a quote and an author, then will display the quotation and author as shown in the example output.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
 
+            string userQuote;
+            string authorOfQuote;
+
+
             Console.WriteLine("Printing Quotes \n");
             Console.WriteLine("This program will prompt for a quote and an author, then will display the quotation and author as shown in the example output. \n");
             Console.WriteLine("For Example: \n\n" +
                 "\t What is your quote? \"You wanna go to jail, or go home, Jake?\" \n" +
                 "\t Who said it? Denzel Washington \n" + "\n\t Denzel Washington said, \"You wanna go to jail or go home, Jake?\"");
             Console.WriteLine("---------------------------------------------------------------------- \n");
+
+            Console.Write("What is your quote? ");
+            userQuote = Console.ReadLine();
+
+            Console.Write("Who said it? ");
+            authorOfQuote = Console.ReadLine();
+
+            Console.WriteLine($"\n{authorOfQuote} said, \"{userQuote}\"");
 
         }
     }
